f1841e945eb1fb517c9521cfaf3e7201d6350ea4
shuq-git <shuq2@illinois.edu>
2025-04-09 11:02:32 -0500

    Update lab4_emulator_client.py
    - Need to update the path names of the certificate and key. Also need the organization ID for our Endpoint.

--- lab4_emulator_client.py
+++ lab4_emulator_client.py
@@ -9,10 +9,11 @@ import numpy as np
 #TODO 1: modify the following parameters
 #Starting and end index, modify this
 device_st = 0
-device_end = 100
+device_end = 5
 
 #Path to the dataset, modify this
-data_path = "vehicle.csv"
+#data_path = "vehicle.csv"
+data_path = "vehicle{}.csv"
 
 #Path to your certificates, modify this
 certificate_formatter = "certificate.pem"
